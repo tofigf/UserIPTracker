71a0120f55536849cec1727001190af5daf3bfd7
Tofig Farajullayev <tofiq.ferecullayev@gmail.com>
2025-03-09 17:45:01 +0400

    seed_test_data

--- seed_test_data.sql
+++ seed_test_data.sql
@@ -1,7 +1,12 @@
-﻿-- Bulk Insert 500,000 Test Records for UserId = 1
+﻿INSERT INTO "Users" ("Id") 
+SELECT generate_series(1, 1000);
 
 INSERT INTO "UserConnections" ("UserId", "IpAddress", "ConnectedAt")
-SELECT 1, '192.168.1.' || (random() * 255)::int, NOW()
-FROM generate_series(1, 500000);
+SELECT 
+    (i % 1000) + 1,
+    '192.168.' || (random() * 255)::int || '.' || (random() * 255)::int,
+    NOW() - (random() * interval '30 days')
+FROM generate_series(1, 500000) AS i;
+
 
 --psql -U postgres -d UserIPTrackerDB -f sql/seed_test_data.sql
